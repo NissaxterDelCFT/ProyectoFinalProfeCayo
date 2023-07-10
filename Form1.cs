349f27453c5f84213623b57aa9250ac44e5d254b
NissaxterDelCFT <carlos.flores.calle@cftmail.cl>
2023-07-09 23:05:33 -0400

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace holasisi
+{
+    public partial class Form1 : Form
+    {
+        static string cadena = "Data Source=.;Initial Catalog=systemcft;Integrated Security=True";
+        SqlConnection cn = new SqlConnection(cadena);
+        SqlCommand com = new SqlCommand();
+        SqlDataReader datos;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            com.Connection = cn;
+            com.CommandType = CommandType.Text;
+            cn.Open();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            com.CommandText = "select nombre from Estudiante where id='" + this.textBox1.Text + "'";
+            datos=com.ExecuteReader();
+            if (datos.HasRows == true) {
+                while (datos.Read()){
+                    this.listBox1.Items.Add(datos.GetString(0));
+                }
+            }
+            else
+            {
+                this.listBox1.Items.Add("Sin resultados");
+
+            }
+            datos.Close();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
